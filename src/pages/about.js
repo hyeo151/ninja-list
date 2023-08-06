d7a1d73a5d3a7a5579f7ebe12221adfd0f83045b
khyeo151@gmail.com <khyeo151@gmail.com>
2023-08-06 13:47:05 +1200

    Initial

---
+++ src/pages/about.js
@@ -0,0 +1,28 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About </title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <h1>About</h1>
+      <p>
+        {" "}
+        temporibus Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Itaque tenetur, expedita libero similique quam reiciendis atque? Sequi
+        tempora earum deleniti incidunt, quas repellat aperiam consequuntur
+        eveniet. Omnis illo officia temporibus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque
+        tenetur, expedita libero similique quam reiciendis atque? Sequi tempora
+        earum deleniti incidunt, quas repellat aperiam consequuntur eveniet.
+        Omnis illo officia.
+      </p>
+    </>
+  );
+};
+
+export default About;
